60ce905ac2858270a4b3285f67e1ac092486fdef
Debojeet Jha <debojeetjha@gmail.com>
2022-11-18 03:15:22 +0530

    Fix latest story fetching

--- src/helpers/getLatestStories.js
+++ src/helpers/getLatestStories.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {BASE_URL_SEARCH_BY_DATE} from '../const';
 
-const getLatestStories = async (page = 0, createdAtLessThan = Date.now()) => {
+const getLatestStories = async (page = 0, createdAtLessThan = Date.now()/1000) => {
   const URL = BASE_URL_SEARCH_BY_DATE +
     `?tags=story&&numericFilters=created_at_i<${createdAtLessThan}&page=${page}`;
   console.log(URL);
